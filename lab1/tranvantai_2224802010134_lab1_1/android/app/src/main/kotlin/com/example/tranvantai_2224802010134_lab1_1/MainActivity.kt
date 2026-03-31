5c81b12fc6db2bae623857c54eeb9f61c7971643
Taiikonn <2k4tranvantai@gmail.com>
2026-03-31 14:08:56 +0700

    cmit
    lab1

---
+++ lab1/tranvantai_2224802010134_lab1_1/android/app/src/main/kotlin/com/example/tranvantai_2224802010134_lab1_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tranvantai_2224802010134_lab1_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab1/tranvantai_2224802010134_lab1_2/test/tranvantai_2224802010134_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:tranvantai_2224802010134_lab1_2/tranvantai_2224802010134_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
