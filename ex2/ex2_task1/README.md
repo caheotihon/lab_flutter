16455c1c24a64c56f2ee6e718f2f25b28c806cd6
Taiikonn <2k4tranvantai@gmail.com>
2026-05-14 22:46:51 +0700

    cmit

---
+++ ex2/ex2_task1/README.md
@@ -0,0 +1,4 @@
+# ex2_task1
+
+## Demo Video
+https://drive.google.com/file/d/15p1Bo3l6sEpK4Ff-paVeD1G2fJt1sK-Y/view?usp=sharing
---
+++ ex2/ex2_task1/android/app/src/main/kotlin/com/example/ex2_task1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ex2_task1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
